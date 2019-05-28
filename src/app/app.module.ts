64ba9d6a9aae40e9909da611ebe88900465bfc93
Yury Tatarynovich <Yury Tatarynovich>
2019-05-28 14:14:56 +0300

    add finger mirror

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,37 +7,26 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import {RealtimeModule} from "./components/realtime/realtime.module";
 import localeRu from '@angular/common/locales/ru';
 import {registerLocaleData} from "@angular/common";
-import {WeatherModule} from "./components/weather/weather.module";
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material';
 import {AppListComponent} from "./components/app-list/app-list.component";
-import {VgCoreModule} from "videogular2/core";
-import {VgControlsModule} from "videogular2/controls";
-import {VgOverlayPlayModule} from "videogular2/overlay-play";
-import {VgBufferingModule} from "videogular2/buffering";
+
+import {HomeModule} from "./components/home/home.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     AppListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    RealtimeModule,
-    WeatherModule,
+    HomeModule,
     MatIconModule,
-    MatGridListModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule
+    MatGridListModule
   ],
   providers: [{provide: LOCALE_ID, useValue: 'ru'}],
   bootstrap: [AppComponent]
---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HomeComponent} from "./home.component";
+import {FingerMirrorComponent} from "../finger-mirror/finger-mirror.component";
+import {RealtimeModule} from "../realtime/realtime.module";
+import {WeatherModule} from "../weather/weather.module";
+import {VgCoreModule} from "videogular2/core";
+import {VgControlsModule} from "videogular2/controls";
+import {VgOverlayPlayModule} from "videogular2/overlay-play";
+import {VgBufferingModule} from "videogular2/buffering";
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    FingerMirrorComponent
+  ],
+  imports: [
+    CommonModule,
+    RealtimeModule,
+    WeatherModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
+  ],
+  exports: [
+    HomeComponent,
+    FingerMirrorComponent
+  ]
+})
+export class HomeModule { }
