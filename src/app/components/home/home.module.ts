2c5bb0e03b003d86776bc77bb346c93df496c2b2
Andrey Ovchinnikov <ovchinnikov@strim-tech.com>
2019-05-29 13:18:29 +0300

    videoplayer now is a module

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,10 +4,7 @@ import {HomeComponent} from "./home.component";
 import {FingerMirrorComponent} from "../finger-mirror/finger-mirror.component";
 import {RealtimeModule} from "../realtime/realtime.module";
 import {WeatherModule} from "../weather/weather.module";
-import {VgCoreModule} from "videogular2/core";
-import {VgControlsModule} from "videogular2/controls";
-import {VgOverlayPlayModule} from "videogular2/overlay-play";
-import {VgBufferingModule} from "videogular2/buffering";
+import {MyVideoModule} from "../myvideo/myvideo.module";
 import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
@@ -20,10 +17,7 @@ import {BrowserModule} from "@angular/platform-browser";
     CommonModule,
     RealtimeModule,
     WeatherModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule
+    MyVideoModule
   ],
   exports: [
     HomeComponent,
---
+++ src/app/components/myvideo/myvideo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'video-player',
+  templateUrl: './myvideo.component.html',
+  styleUrls: ['./myvideo.component.scss']
+})
+export class MyVideoComponent{
+
+
+}
