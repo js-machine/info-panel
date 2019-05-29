6756622a291304166abf8c6507089cc7cbf9cc8e
Andrey Ovchinnikov <ovchinnikov@strim-tech.com>
2019-05-29 14:36:40 +0300

    added playlist to video player

--- src/app/components/myvideo/myvideo.component.ts
+++ src/app/components/myvideo/myvideo.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { VgAPI } from 'videogular2/core';
+
+export interface IMedia {
+  title: string;
+  src: string;
+  type: string;
+}
 
 @Component({
   selector: 'video-player',
   templateUrl: './myvideo.component.html',
   styleUrls: ['./myvideo.component.scss']
 })
-export class MyVideoComponent{
 
 
+export class MyVideoComponent {
+  playlist: Array<IMedia> = [
+    {
+      title: 'STRIM',
+      src: '../../../assets/1.mp4',
+      type: 'video/mp4'
+    },
+    {
+      title: 'Mogilev',
+      src: '../../../assets/2.mp4',
+      type: 'video/mp4'
+    }
+  ];
+
+  currentIndex = 0;
+  currentItem: IMedia = this.playlist[this.currentIndex];
+  api: VgAPI;
+
+  constructor() {
+  }
+
+  onPlayerReady(api: VgAPI) {
+    this.api = api;
+    this.api.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.playVideo.bind(this));
+    this.api.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
+  }
+
+  nextVideo() {
+    console.log("test");
+    this.currentIndex++;
+
+    if (this.currentIndex === this.playlist.length) {
+      this.currentIndex = 0;
+    }
+
+    this.currentItem = this.playlist[this.currentIndex];
+  }
+
+  playVideo() {
+    this.api.play();
+  }
 }
--- src/app/components/myvideo/myvideo.module.ts
+++ src/app/components/myvideo/myvideo.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
-import {MyVideoComponent} from "./myvideo.component";
 import { CommonModule } from '@angular/common';
-import {VgCoreModule} from "videogular2/core";
-import {VgControlsModule} from "videogular2/controls";
-import {VgOverlayPlayModule} from "videogular2/overlay-play";
-import {VgBufferingModule} from "videogular2/buffering";
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgBufferingModule } from 'videogular2/buffering';
+import { MyVideoComponent } from "./myvideo.component";
 
 @NgModule({
   declarations: [
     MyVideoComponent
   ],
   imports: [
-    HttpClientModule,
-    HttpClientJsonpModule,
     CommonModule,
     VgCoreModule,
     VgControlsModule,
-    VgOverlayPlayModule,
     VgBufferingModule
   ],
   exports: [
