560cd8e3b21ab1cab48090126de799f434330a8f
Andrey Ovchinnikov <ovchinnikov@strim-tech.com>
2019-07-05 12:20:55 +0300

    feat: change commit header length rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    "header-max-length": [1, "always", 72],
+    'header-max-length': [1, 'always', 72],
     'type-enum': [
       2,
       'always',
